436a15ebcd2b6cd11c9b8a7d8e984f671c58ae29
patrickseow <patrickseowtecksoon@gmail.com>
2020-09-07 05:39:05 +0800

    no message

--- main.a32dc.js
+++ main.a32dc.js
@@ -141,7 +141,7 @@ window.boot = function () {
     // jsList
     var jsList = settings.jsList;
 
-    var bundledScript = settings.debug ? 'src/project.dev.js' : 'src/project.270b2.js';
+    var bundledScript = settings.debug ? 'src/project.dev.js' : 'src/project.c5c7d.js';
     if (jsList) {
         jsList = jsList.map(function (x) {
             return 'src/' + x;
